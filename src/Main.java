317ab28baa6d8e422aafb32c8cdf580108614963
pavilion <jakisfajnyktos@gmail.com>
2024-10-10 19:00:29 +0200

    feat/c1

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,9 @@ public class Main {
         System.out.println("Hello and welcome!");
         System.out.println("Hello and welcome2!");
         System.out.println("Hello and welcome3!");
+        System.out.println("Hello and welcome4!");
+
+
 
     }
 }
